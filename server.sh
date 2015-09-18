ec839ca5404b2122d6e6f278b072166637e3936a
Kirill Popov <kpopov@griddynamics.com>
2015-09-18 15:22:38 +0300

    added content and server scripts

---
+++ server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    netcat -l -n -p $PORT -q 1 -v << ENDOFREPLY
+HTTP-Version: HTTP/1.0 200 OK
+Content-Length: $(stat -c %s content.html)
+Content-Type: text/html
+
+$(cat content.html)
+ENDOFREPLY
+done

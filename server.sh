86d82919356af5a715a23b2c969ceb44098a091a
Kirill Popov <kpopov@griddynamics.com>
2015-09-18 16:29:07 +0300

    made netcat wait forever

--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@
 PORT=${PORT:-8000}
 
 while true; do
-    netcat -l -n -p $PORT -q 1 -v << ENDOFREPLY
+    netcat -l -n -p $PORT -q -1 -v << ENDOFREPLY
 HTTP-Version: HTTP/1.0 200 OK
 Content-Length: $(stat -c %s content.html)
 Content-Type: text/html

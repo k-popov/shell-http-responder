92f8f40ec8a674bf815d96dc4e3c920c21f42d7e
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 12:57:59 +0300

    fixed syntax

--- server.sh
+++ server.sh
@@ -3,6 +3,6 @@
 PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
-while true do
+while true; do
     socat -v tcp-l:${PORT},reuseaddr,crlf EXEC:'bash responder.sh'
 done

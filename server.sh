3296c80c94825494358aa0d8796cfa0f1ab11059
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 16:58:07 +0300

    forked socat again

--- server.sh
+++ server.sh
@@ -4,5 +4,5 @@ PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
 while [ "$?" -eq "0" ]; do
-    socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf EXEC:'bash responder.sh'
+    socat -d -d -d -v TCP4-LISTEN:${PORT},reuseaddr,crlf,fork EXEC:'bash responder.sh',pipes
 done

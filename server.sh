989df136d55495f8323e268ebf930ffdf34db1d8
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 14:01:58 +0300

    used fifo approach

--- server.sh
+++ server.sh
@@ -4,6 +4,8 @@ PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
 while [ "0" -eq "0" ]; do
-    socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf SYSTEM:'bash responder.sh',nofork
+    rm -f fifo
+    mkfifo fifo
+    bash responder.sh < fifo | socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf - > fifo
     echo "Socat exit code is $?"
 done

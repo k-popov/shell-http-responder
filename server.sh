7c43fce24547ce3f3e97c95a4b58068a7d2dea5c
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 12:44:44 +0300

    made socat verbose

--- server.sh
+++ server.sh
@@ -4,5 +4,5 @@ PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
 while [ "$?" -eq "0" ]; do
-    socat tcp-l:${PORT},reuseaddr,crlf EXEC:'bash responder.sh'
+    socat -v tcp-l:${PORT},reuseaddr,crlf EXEC:'bash responder.sh'
 done

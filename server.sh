ccc3fcc4ec6273d3613bdef7d586cbed6083d793
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 13:51:13 +0300

    returned nofork, removed pipes

--- server.sh
+++ server.sh
@@ -3,6 +3,7 @@
 PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
-while [ "$?" -eq "0" ]; do
-    socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf SYSTEM:'bash responder.sh',pipes
+while [ "0" -eq "0" ]; do
+    socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf SYSTEM:'bash responder.sh',nofork
+    echo "Socat exit code is $?"
 done

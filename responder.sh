3e7bee9737dbbec46f8f3b9ebec2893c858fa244
Kirill Popov <kpopov@griddynamics.com>
2015-09-21 16:46:42 +0300

    returned to an old, forked socat model

--- responder.sh
+++ responder.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
-    r=read;
-    e=echo;
-    echo "a=$a b=$b c=$c" 1>&2
-    $r a b c;
-    z=$r;
-    echo "z=$z" 1>&2
-    while [ ${#z} -gt 2 ]; do
-        $r z;
-    done;
-    f=`$e $b|sed 's/[^a-z0-9_.-]//gi'`;
-    h="HTTP/1.0";
-    o="$h 200 OK\r\n";
-    c="Content";
-    if [ -z "$f" ];then (
-        $e $o;
-        (for n in *;do
-            if [ -f "$n" ]; then
-                $e "<a href=\"/$n\">`ls -gh \"$n\"`</a><br>";
-            fi;
-        done);
-    );
-    elif [ -f "$f" ];then
-        $e "$o$c-Type: `file -ib \"$f\"`\n$c-Length: `stat -c%s \"$f\"`";
-        $e;
-        $e $f>&2;
-        cat "$f";
-    else
-        $e -e "$h 404 Not Found\n\n404\n";
-    fi
+r=read;
+e=echo;
+$r a b c;
+z=$r;
+while [ ${#z} -gt 2 ]; do
+    $r z;
+done;
+f=`$e $b|sed 's/[^a-z0-9_.-]//gi'`;
+h="HTTP/1.0";
+o="$h 200 OK\r\n";
+c="Content";
+if [ -z "$f" ];then (
+    $e $o;
+    (for n in *;do
+        if [ -f "$n" ]; then
+            $e "<a href=\"/$n\">`ls -gh \"$n\"`</a><br>";
+        fi;
+    done);
+);
+elif [ -f "$f" ];then
+    $e "$o$c-Type: `file -ib \"$f\"`\n$c-Length: `stat -c%s \"$f\"`";
+    $e;
+    $e $f>&2;
+    cat "$f";
+else
+    $e -e "$h 404 Not Found\n\n404\n";
+fi
--- server.sh
+++ server.sh
@@ -3,9 +3,6 @@
 PORT=${PORT:-8000}
 
 # code from https://gist.github.com/alexey-sveshnikov/69d502aefd05a539c165
-while [ "0" -eq "0" ]; do
-    rm -f fifo
-    mkfifo fifo
-    bash responder.sh < fifo | socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf - > fifo
-    echo "Socat exit code is $?"
+while [ "$?" -eq "0" ]; do
+    socat -d -d -d -v tcp-l:${PORT},reuseaddr,crlf EXEC:'bash responder.sh'
 done
